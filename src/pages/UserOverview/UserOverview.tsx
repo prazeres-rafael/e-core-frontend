c41bab26383474447c00385619932eb8c9abfb7f
Rafael Prazeres da Silva <rafaelpraz.adm@gmail.com>
2023-05-07 20:15:44 -0300

    refactor: improve use overview page
    1. The import for React is simplified.
    
    2. The variable mapU is renamed to mapUserToCard to provide a more
    descriptive name that reflects its purpose.
    
    3. Destructuring assignment is used to extract the firstName and
    lastName properties from location.state to improve readability.
    
    4. The Header component title is constructed using template literals
    for better readability.
    
    5. The Card component is rendered directly using the mapUserToCard
    function for improved clarity.

---
+++ src/pages/UserOverview/UserOverview.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {useLocation} from 'react-router-dom';
+import {UserData} from 'types';
+
+import {Card, Header} from '../../components';
+
+const mapUserToCard = (user: UserData) => {
+    const columns = [
+        {
+            key: 'Name',
+            value: `${user.firstName} ${user.lastName}`,
+        },
+        {
+            key: 'Display Name',
+            value: user.displayName,
+        },
+        {
+            key: 'Location',
+            value: user.location,
+        },
+    ];
+    return <Card columns={columns} hasNavigation={false} navigationProps={user} />;
+};
+
+const UserOverview = () => {
+    const location = useLocation();
+    const {firstName, lastName} = location.state;
+
+    return (
+        <React.Fragment>
+            <Header title={`User ${firstName} ${lastName}`} />
+            {mapUserToCard(location.state)}
+        </React.Fragment>
+    );
+};
+
+export default UserOverview;
